9244227ace5975f6648dac0f8710f71af65fbfd2
ben23515225566 <ben23515225566@gmail.com>
2024-07-29 15:18:49 +0800

    [Feature: command line parameter] It can use command line parameter to specific what directory will be retrieved.

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 This project just want to do something with rust language. 
 
+## Description
+
 It can retrieve all of the file names in the directory. Simply place the binary file in the target directory and run it. You will then get an output file named **file_names.txt**.
 
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,54 @@
+use core::panic;
 use std::{env, fs, io::Write};
 
+struct Config{
+    dir_path: String,
+}
+
+impl Config {
+    fn new(path: String) -> Config {
+        Config {
+            dir_path: path,
+        }
+    }
+}
+
 fn main() {
-    let current_dir = env::current_dir().expect("Failed to get current directory");
-    let current_dir = current_dir.as_os_str().to_str().unwrap();
-    dbg!(current_dir);
+    let args: Vec<String> = env::args().collect();
+    let path = parse_args(&args);
 
-    let directory_path = "./test_dir";
+    println!("Read path: {}", path);
 
-    let file_list = read_file_name(current_dir);
-    
+    let config = Config::new(path);
+    let file_list = read_file_name(&config);
     write_file(&file_list);
 }
 
-fn read_file_name(directory_path: &str) -> Vec<String> {
+fn parse_args(args: &Vec<String>) -> String {
+    let path: String;
+
+    if args.len() > 2{
+        panic!("Error: Only one parameter is allowed !");
+    } else if args.len() == 2{
+        path = args[1].clone();
+    } else {
+        let current_dir = env::current_dir().expect("Failed to get current directory");
+        let current_dir = current_dir.as_os_str().to_str().unwrap().to_string();
+        path = current_dir
+    }
+
+    path
+}
+
+fn read_file_name(config: &Config) -> Vec<String> {
+    let directory_path = &config.dir_path;
     let entries = fs::read_dir(directory_path).expect("Failed to read directory.");
     let mut list = Vec::new();
 
     for entry in entries {
-        match entry {
-            Ok(file) => {
-                let file_name = file.file_name().into_string().unwrap();
+        if let Ok(entry) = entry {
+            let file_name = entry.file_name().into_string().unwrap();
                 list.push(file_name);
-            }
-            Err(err) => {
-                let error = err;
-                dbg!(error);
-            }
         }
     }
 
@@ -33,16 +56,12 @@ fn read_file_name(directory_path: &str) -> Vec<String> {
     list
 }
 
-fn write_file(file_list: &Vec<String>){
+fn write_file(file_list: &Vec<String>) {
     let mut file = fs::File::create("file_names.txt").expect("Failed to create file.");
-    for file_name in file_list{
-        file.write_all(file_name.as_bytes()).expect("Failed to write file.");
-        file.write_all("\n".as_bytes()).expect("Failed to write new line.");
-    }
-}
-
-fn print_file_list(file_list: &Vec<String>){
-    for file in file_list{
-        println!("{}", file);
+    for file_name in file_list {
+        file.write_all(file_name.as_bytes())
+            .expect("Failed to write file.");
+        file.write_all("\n".as_bytes())
+            .expect("Failed to write new line.");
     }
 }
